940f2026f20524621371b220abd656fd6b161fb7
Leevi Puntanen <leevi.puntanen@ksyk.fi>
2023-03-07 13:14:21 +0200

    Terve vaan taas

--- src/index.jsx
+++ src/index.jsx
@@ -7,155 +7,122 @@ import './assets/stylesheets/style.css'
 
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
-import React, {Component} from "react";
-import "./App.css"
+import { useState } from 'react';
 
-const  HEIGHT = 10;
-const  WIDTH  = 10;
-
-// mapping keycode  for changing direction
-const LEFT  = 37; 
-const UP    = 38;
-const RIGHT = 39; 
-const DOWN  = 40;
-const STOP  = 32; /* [space] used for pause */
-
-const getRandom = () => {
-    return  { 
-        x: Math.floor(Math.random() *WIDTH),
-        y: Math.floor(Math.random() *HEIGHT) 
-    }
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
-const emptyRows = () => [...Array(WIDTH)].map((_) => [...Array(HEIGHT)].map((_)=> 'grid-item'));
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
 
-const increaseSpeed = (speed) => speed - 10 *(speed > 10)
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
 
-const initialState = {
-    rows: emptyRows(),
-    snake: [getRandom()],
-    food: getRandom(),
-    direction: STOP,
-    speed: 100,
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
 
-class App extends Component {
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
-    constructor() {
-        super();
-        this.state = initialState;
-    }
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
 
-    componentDidMount() {
-        setInterval(this.moveSnake, this.state.speed);
-        document.onkeydown = this.changeDirection;
-        document.title = "snake-game";
-    }
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
 
-    componentDidUpdate() {
-        this.isCollapsed();
-        this.isEaten();
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
     }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
-    moveSnake = () => {
-        let snakeCopy = [...this.state.snake];
-        let head  =  {...snakeCopy[snakeCopy.length-1]};
-        switch (this.state.direction) {
-            case LEFT:  head.y += -1; break;    
-            case UP:    head.x += -1; break;
-            case RIGHT: head.y += 1;  break;
-            case DOWN:  head.x += 1;  break;
-            default: return;
-        }
-        /* keep the value within range of 0 to HEIGHT */
-        head.x += HEIGHT * ((head.x<0)-(head.x>=HEIGHT));
-        head.y += WIDTH * ((head.y<0)-(head.y>=WIDTH));
-        
-        snakeCopy.push(head); 
-        snakeCopy.shift()
-        this.setState({
-            snake: snakeCopy,
-            head: head
-        });
-        this.update(); 
-    }   
-    
-    isEaten() {
-        let snakeCopy  = [...this.state.snake];
-        let head  =  {...snakeCopy[snakeCopy.length-1]};
-        let food = this.state.food;
-        if ((head.x === food.x) &&(head.y === food.y)) {
-            snakeCopy.push(head);
-            this.setState({
-                snake: snakeCopy,
-                food: getRandom(),
-                speed: increaseSpeed(this.state.speed) 
-            });
-        } 
-    }
-
-    update() {
-        let newRows = emptyRows(); 
-        this.state.snake.forEach(element => newRows[element.x][element.y] = 'snake')
-        newRows[this.state.food.x][this.state.food.y] = 'food';
-        this.setState({rows: newRows});
-    }
-
-    isCollapsed = () => {
-        let snake = this.state.snake;
-        let head  = {...snake[snake.length-1]} 
-        for (let i=0; i<snake.length-3; i++) {
-            if ((head.x === snake[i].x) &&(head.y === snake[i].y)) {
-                this.setState(initialState);
-                alert(`game over: ${snake.length*10}`)
-            }
-        }
-    }
-
-    changeDirection = ({keyCode}) => { 
-        let direction = this.state.direction;
-        switch (keyCode) {
-            case LEFT:
-                direction = (direction === RIGHT)? RIGHT: LEFT;
-                break;
-            case RIGHT:
-                direction = (direction === LEFT)? LEFT: RIGHT;
-                break;
-            case UP:
-                direction = (direction === DOWN)? DOWN: UP;
-                break;
-            case DOWN:
-                direction = (direction === UP)? UP: DOWN;
-                break;
-            case STOP:
-                direction = STOP;
-                break;
-            default:
-                break;
-        }
-        this.setState({
-            direction: direction
-        });
-    }    
-   
-    render() {
-        const displayRows = this.state.rows.map((row, i) => row.map((value, j) =>  <div name={`${i}=${j}`} className={value} />))
-        return (
-            <div className="a">
-                <h1> Snake  v0.1.1</h1>
-                <ul>
-                    <li>press "space" to pause the game.</li>
-                    <li>press "arrow keys" to change direction/ unpause.</li>
-                </ul>
-                <div className="snake-container">
-                    <div className="grid">{displayRows}</div>
-                </div>
-            </div>
-        )    
-    }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
 
-export default App;
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 
 /****** DO NOT DELETE AFTER THIS LINE ******/
